44f95fad4c447452a24c446fedfcbd960402a1ad
Dominik Frolík <domin83@hotmail.cz>
2020-10-19 18:59:57 +0200

    Založení calculatoru a root cest na home

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {    return view('welcome');});
+
+Route::get('home', function () {    return view('home');});
+
+//Route::get('calculator', function () {    return view('calculator');});
+
+Route::get('calculator', 'CalculatorController@index');
