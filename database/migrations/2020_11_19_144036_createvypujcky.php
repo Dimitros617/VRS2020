caf5a5782d3da860ec79700710283f704888d1a4
Darkain <dofu2245@gmail.com>
2020-11-19 15:57:48 +0100

    databaze (ver. 1.1)

---
+++ database/migrations/2020_11_19_144036_createvypujcky.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVypujcky extends Migration
+{
+    public function up()
+    {
+        Schema::create('vypujcky', function (Blueprint $table) {
+
+            $table->id();
+            $table->integer('uzivatel');
+            $table->integer('item');
+            $table->timestamp('vypujceno_od')->useCurrent();
+            $table->timestamp('vypujceno_do')->useCurrent();
+            $table->tinyInteger('stav')->default('1');
+
+
+
+        });
+    }
+
+
+}
+
