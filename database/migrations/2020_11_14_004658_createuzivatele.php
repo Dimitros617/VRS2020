bf40a2275d98b238c5d58f79ff97afee16845c05
Darkain <dofu2245@gmail.com>
2020-11-19 15:53:32 +0100

    databaze (ver. 1.1)

--- database/migrations/2020_11_14_004658_createuzivatele.php
+++ database/migrations/2020_11_14_004658_createuzivatele.php
@@ -4,25 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Createuzivatele extends Migration
+class CreateUzivatele extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        //
+        Schema::create('uzivatele', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('jmeno_uzivatele',40);
+            $table->string('prijmeni_uzivatele',40);
+            $table->string('heslo',40);
+            $table->integer('telefon');
+            $table->string('email',40);
+            $table->tinyInteger('verify')->default('0');
+            $table->integer('permition');
+
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+
 }
