142f6ae5adc1c20df931ae8dbdc749dc4dfbaadc
LivvW <vivrbka@seznam.cz>
2020-11-20 13:43:50 +0100

    Poupraven route, kosmetická úprava pro mě

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControllerUsing;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,15 +11,13 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|php
 */
 
 Route::get('/', function () {    return view('welcome');});
 
-Route::get('home', function () {    return view('home');});
 
-Route::get('main', 'App\Http\Controllers\ControllerUsing@main'); //cesta ke classe, ve které je metoda s view
+//Route::get('main', 'App\Http\Controllers\ControllerUsing@main'); //cesta ke classe, ve které je metoda s view
+Route::get('main',[ControllerUsing::class,'main']);
 
-
-//Route::get('calculator', function () {    return view('calculator');});
 
