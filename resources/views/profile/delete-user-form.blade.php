d659825c0ed1ed253d65cbf373c26427bb41b558
LivvW <vivrbka@seznam.cz>
2020-12-02 19:50:48 +0100

    Přeložena záložka nastavení, základní viditelné věci

--- resources/views/profile/delete-user-form.blade.php
+++ resources/views/profile/delete-user-form.blade.php
@@ -1,94 +1,50 @@
 <x-jet-action-section>
     <x-slot name="title">
-        {{ __('Relace v prohlížečích') }}
+        {{ __('Odstranění účtu') }}
     </x-slot>
 
     <x-slot name="description">
-        {{ __('Spravujte své aktivní relace v ostatních prohlížečích či na jiných zařízení, případně se z nich odhlašte.') }}
+        {{ __('Trvalé smazání Vašeho účtu') }}
     </x-slot>
 
     <x-slot name="content">
         <div class="max-w-xl text-sm text-gray-600">
-            {{ __('Pokud je to nutné, zde se můžete odhlásit ze všech ostatních zařízení či prohlížečů, kde jste zůstali přihlášeni. Některé z těchto relací jsou vypsány níže, tento seznam však nemusí být kompletní. Pokud máte pocit, že byl Váš účet ohrožen, zároveň byste měli aktualizovat i své heslo.') }}
+            {{ __('Poté, co smažete Váš účet, všechna Vaše data budou nenávratně smazána. Předtím, než toto uděláte, uschovejte si informace, které byste chtěli zachovat pro pozdější použití.') }}
         </div>
 
-        @if (count($this->sessions) > 0)
-            <div class="mt-5 space-y-6">
-                <!-- Other Browser Sessions -->
-                @foreach ($this->sessions as $session)
-                    <div class="flex items-center">
-                        <div>
-                            @if ($session->agent->isDesktop())
-                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="w-8 h-8 text-gray-500">
-                                    <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
-                                </svg>
-                            @else
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-8 h-8 text-gray-500">
-                                    <path d="M0 0h24v24H0z" stroke="none"></path><rect x="7" y="4" width="10" height="16" rx="1"></rect><path d="M11 5h2M12 17v.01"></path>
-                                </svg>
-                            @endif
-                        </div>
-
-                        <div class="ml-3">
-                            <div class="text-sm text-gray-600">
-                                {{ $session->agent->platform() }} - {{ $session->agent->browser() }}
-                            </div>
-
-                            <div>
-                                <div class="text-xs text-gray-500">
-                                    {{ $session->ip_address }},
-
-                                    @if ($session->is_current_device)
-                                        <span class="text-green-500 font-semibold">{{ __('Toto zařízení') }}</span>
-                                    @else
-                                        {{ __('Naposledy připojen') }} {{ $session->last_active }}
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        @endif
-
-        <div class="flex items-center mt-5">
-            <x-jet-button wire:click="confirmLogout" wire:loading.attr="disabled">
-                {{ __('Odhlásit z ostatních relací') }}
-            </x-jet-button>
-
-            <x-jet-action-message class="ml-3" on="loggedOut">
-                {{ __('Hotovo.') }}
-            </x-jet-action-message>
+        <div class="mt-5">
+            <x-jet-danger-button wire:click="confirmUserDeletion" wire:loading.attr="disabled">
+                {{ __('Smazat účet') }}
+            </x-jet-danger-button>
         </div>
 
-        <!-- Logout Other Devices Confirmation Modal -->
-        <x-jet-dialog-modal wire:model="confirmingLogout">
+        <!-- Delete User Confirmation Modal -->
+        <x-jet-dialog-modal wire:model="confirmingUserDeletion">
             <x-slot name="title">
-                {{ __('Odhlásit z ostatních relací') }}
+                {{ __('Smazat účet') }}
             </x-slot>
 
             <x-slot name="content">
-                {{ __('Prosím, zadejte své heslo jako potvrzení, že chcete odhlásit z jiných relací v prohlížečích na všech Vašich zařízení.') }}
+                {{ __('Jste si jistý, že chcete odstranit Váš účet? Poté, co smažete Váš účet, všechna Vaše data budou nenávratně smazána. Prosím, zadejte své heslo jako potvrzení, že chcete trvale a nenávratně odstranit Váš účet.') }}
 
-                <div class="mt-4" x-data="{}" x-on:confirming-logout-other-browser-sessions.window="setTimeout(() => $refs.password.focus(), 250)">
+                <div class="mt-4" x-data="{}" x-on:confirming-delete-user.window="setTimeout(() => $refs.password.focus(), 250)">
                     <x-jet-input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Heslo') }}"
-                                 x-ref="password"
-                                 wire:model.defer="password"
-                                 wire:keydown.enter="logoutOtherBrowserSessions" />
+                                x-ref="password"
+                                wire:model.defer="password"
+                                wire:keydown.enter="deleteUser" />
 
                     <x-jet-input-error for="password" class="mt-2" />
                 </div>
             </x-slot>
 
             <x-slot name="footer">
-                <x-jet-secondary-button wire:click="$toggle('confirmingLogout')" wire:loading.attr="disabled">
+                <x-jet-secondary-button wire:click="$toggle('confirmingUserDeletion')" wire:loading.attr="disabled">
                     {{ __('Nyní ne') }}
                 </x-jet-secondary-button>
 
-
-                <x-jet-button class="ml-2" wire:click="logoutOtherBrowserSessions" wire:loading.attr="disabled">
-                    {{ __('Odhlásit z ostatních relací') }}
-                </x-jet-button>
+                <x-jet-danger-button class="ml-2" wire:click="deleteUser" wire:loading.attr="disabled">
+                    {{ __('Smazat účet') }}
+                </x-jet-danger-button>
             </x-slot>
         </x-jet-dialog-modal>
     </x-slot>
