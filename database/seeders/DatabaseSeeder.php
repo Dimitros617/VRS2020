21e77980dfa0b318958e43be32d772fc26731812
Darkain <dofu2245@gmail.com>
2020-12-06 14:36:15 +0100

    zaznamy pro roli permition - uprava 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('permition')->insert([
+            'id' => 1,
+            'name' => 'unverifiedUser',
+            'possibility_renting' => 0,
+            'new_user' => 0,
+            'return_verification' => 0,
+            'edit_item' => 0,
+        ]);
+        DB::table('permition')->insert([
+            'id' => 2,
+            'name' => 'verifiedUser',
+            'possibility_renting' => 1,
+            'new_user' => 0,
+            'return_verification' => 0,
+            'edit_item' => 0,
+        ]);
+        DB::table('permition')->insert([
+            'id' => 3,
+            'name' => 'Admin',
+            'possibility_renting' => 1,
+            'new_user' => 1,
+            'return_verification' => 1,
+            'edit_item' => 1,
+        ]);
     }
 }
