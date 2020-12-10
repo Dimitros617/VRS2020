26306b66decbc8719915e55b42b90f78bce98b2f
LivvW <vivrbka@seznam.cz>
2020-12-10 21:12:00 +0100

    Blblosti s DatabaseSeederem

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('permition')->insert([
+            'id' => 1,
+            'name' => 'unverifiedUser',
+            'possibility_renting' => 0,
+            'new_user' => 0,
+            'return_verification' => 0,
+            'edit_item' => 0,
+            'edit_permitions' => 0,
+        ]);
+        DB::table('permition')->insert([
+            'id' => 2,
+            'name' => 'verifiedUser',
+            'possibility_renting' => 1,
+            'new_user' => 0,
+            'return_verification' => 0,
+            'edit_item' => 0,
+            'edit_permitions' => 0,
 
+        ]);
+        DB::table('permition')->insert([
+            'id' => 3,
+            'name' => 'Admin',
+            'possibility_renting' => 1,
+            'new_user' => 1,
+            'return_verification' => 1,
+            'edit_item' => 1,
+            'edit_permitions' => 1,
+
+        ]);
     }
 }
