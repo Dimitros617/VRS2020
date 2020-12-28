b627da29fbc4dc9881edaded2f425474dcbc2bb1
LivvW <vivrbka@seznam.cz>
2020-12-28 18:30:38 +0100

    Položen základ dashboardu

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,17 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class DashboardController extends Controller
 {
     function show()
     {
-        $vypujcky_pocet_d = 'Zatím 0';  //kontrolní hodnota pro zobrazení
-        $schvaleni_pocet_d = 'Zatím 0';
+        $activeLoans = DB::table('loans')->where('loans.user', Auth::user()->id)->where('status', 1)->get();
+        $waitingLoans = DB::table('loans')->where('status', 2)->get();
+
+        $vypujcky_pocet =count($activeLoans);
+        $schvaleni_pocet = count($waitingLoans);
+
 
         Log::info('DashboardControler:show');
-        return view( 'dashboard',['vypujcky_pocet' => $vypujcky_pocet_d, 'schvaleni_pocet' => $schvaleni_pocet_d]);
+        return view( 'dashboard',['vypujcky_pocet' => $vypujcky_pocet, 'schvaleni_pocet' => $schvaleni_pocet]);
 
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,9 +11,50 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{--  Sekce pro Admin, verifiedUser  --}}
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div>Výpůjček: {{ $vypujcky_pocet }}</div>
-                <div>Čeká na schválení: {{ $schvaleni_pocet }}</div>
+                <a href="/loans"><div>Mé aktivní výpůjčky a rezervace: </a><div>{{ $vypujcky_pocet }}</div></div>
+            <a href="/all-loans"><div class="">Čeká na schválení: Pouze pro admina!</a><div>{{ $schvaleni_pocet }}</div></div>
+            </div>
+            <br>
+            {{--  Sekce pro VŠECHNY  --}}
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h2 class="">O systému</h2>
+                <div class="">
+                    Výpůjční a rezervační systém bla bla bla
+                </div>
+            </div>
+            <br>
+
+
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h2 class="">Jak to funguje?</h2>
+                <div class=""> {{--  Sekce pro unverified  --}}
+                    Nyní nemáte právo zapůjčovat položky. Můžete pouze:
+                    <ul style="list-style-type:circle">
+                        <li>Změnit své osobní údaje v záložce "Nastavení".</li>
+                        <li>Pokud jste si již dříve něco půjčil a výpůjčku jste stále nevrátil, jejich seznam najdete v záložce "Moje výpůjčky".</li>
+                        <li>Odhlásit se.</li>
+                    </ul>
+                </div>
+                <div class=""> {{--  Sekce pro verified  --}}
+                    Sekce pro verified user. Bla bla věci JAK si vypůjčit v sekci Nová výpůjčka, JAK vrátit výpůjčku a PROČ je tam čekání na schválení, ikonky upozornění...
+                </div>
+                <div class=""> {{--  Sekce pro Admin  --}}
+                    Sekce pro Admin. Stručný manuál, možná by stálo za to udělat další blade, na kterém bude podrobnější návod pro Admina, na něj odtud odkaz.
+                </div>
+            </div>
+
+            <br>
+            {{--  Sekce pro unverifiedUser  --}}
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h2 class="">Proč si nemohu nic půjčit?</h2>
+                <div class="">
+                    <ul style="list-style-type:circle">
+                        <li>S největší pravděpodobností jste nový uživatel Výpůjčního a rezervačního systému. Musíte počkat, než vás schválí a ověří administrátor.</li>
+                        <li>Pokud jste již jednou schváleni byli a nyní se vám neobjevuje záložka "Nová výpůjčka", bylo vám právo půjčovat si věci odebráno - ať již z důvodu nevrácení výpůjčky včas či jiných problémů. Pro více informací se zeptejte správce systému, administrátora.</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
