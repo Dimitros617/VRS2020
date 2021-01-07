77981514fa194009f8d5eec59234e24fe669f2bb
LivvW <vivrbka@seznam.cz>
2021-01-07 16:36:22 +0100

    Revert "Na nepřihlášených obrazovkách (login, register, forgot, reset, verify, two-factor) sesunuto logo do divu, přidán i další pro komplexní úpravu"
    This reverts commit 37f4a95a

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -1,62 +1,60 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-        </x-slot>
-        <div class="">
-            <div style="">
+            <div style="transform: scale(2); margin-bottom: 25px">
                 {{--            <x-jet-authentication-card-logo />--}}
-                <a href="/"><x-jet-application-logo class="block h-9 w-auto " /></a>
+                <x-jet-application-logo class="block h-9 w-auto " />
             </div>
-            <div x-data="{ recovery: false }">
+        </x-slot>
 
-                <div class="mb-4 text-sm text-gray-600" x-show="! recovery">
-                    {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+        <div x-data="{ recovery: false }">
+            <div class="mb-4 text-sm text-gray-600" x-show="! recovery">
+                {{ __('Please confirm access to your account by entering the authentication code provided by your authenticator application.') }}
+            </div>
+
+            <div class="mb-4 text-sm text-gray-600" x-show="recovery">
+                {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
+            </div>
+
+            <x-jet-validation-errors class="mb-4" />
+
+            <form method="POST" action="/two-factor-challenge">
+                @csrf
+
+                <div class="mt-4" x-show="! recovery">
+                    <x-jet-label for="code" value="{{ __('Code') }}" />
+                    <x-jet-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
                 </div>
 
-                <div class="mb-4 text-sm text-gray-600" x-show="recovery">
-                    {{ __('Please confirm access to your account by entering one of your emergency recovery codes.') }}
+                <div class="mt-4" x-show="recovery">
+                    <x-jet-label for="recovery_code" value="{{ __('Recovery Code') }}" />
+                    <x-jet-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
                 </div>
 
-                <x-jet-validation-errors class="mb-4" />
+                <div class="flex items-center justify-end mt-4">
+                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                                    x-show="! recovery"
+                                    x-on:click="
+                                        recovery = true;
+                                        $nextTick(() => { $refs.recovery_code.focus() })
+                                    ">
+                        {{ __('Use a recovery code') }}
+                    </button>
 
-                <form method="POST" action="/two-factor-challenge">
-                    @csrf
+                    <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
+                                    x-show="recovery"
+                                    x-on:click="
+                                        recovery = false;
+                                        $nextTick(() => { $refs.code.focus() })
+                                    ">
+                        {{ __('Use an authentication code') }}
+                    </button>
 
-                    <div class="mt-4" x-show="! recovery">
-                        <x-jet-label for="code" value="{{ __('Code') }}" />
-                        <x-jet-input id="code" class="block mt-1 w-full" type="text" inputmode="numeric" name="code" autofocus x-ref="code" autocomplete="one-time-code" />
-                    </div>
-
-                    <div class="mt-4" x-show="recovery">
-                        <x-jet-label for="recovery_code" value="{{ __('Recovery Code') }}" />
-                        <x-jet-input id="recovery_code" class="block mt-1 w-full" type="text" name="recovery_code" x-ref="recovery_code" autocomplete="one-time-code" />
-                    </div>
-
-                    <div class="flex items-center justify-end mt-4">
-                        <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
-                                        x-show="! recovery"
-                                        x-on:click="
-                                            recovery = true;
-                                            $nextTick(() => { $refs.recovery_code.focus() })
-                                        ">
-                            {{ __('Use a recovery code') }}
-                        </button>
-
-                        <button type="button" class="text-sm text-gray-600 hover:text-gray-900 underline cursor-pointer"
-                                        x-show="recovery"
-                                        x-on:click="
-                                            recovery = false;
-                                            $nextTick(() => { $refs.code.focus() })
-                                        ">
-                            {{ __('Use an authentication code') }}
-                        </button>
-
-                        <x-jet-button class="ml-4">
-                            {{ __('Login') }}
-                        </x-jet-button>
-                    </div>
-                </form>
-            </div>
+                    <x-jet-button class="ml-4">
+                        {{ __('Login') }}
+                    </x-jet-button>
+                </div>
+            </form>
         </div>
     </x-jet-authentication-card>
 </x-guest-layout>
