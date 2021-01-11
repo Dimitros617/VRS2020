4694be5e41c832db58e1239605137b279115ff7f
katka <killerbunny@post.cz>
2021-01-11 20:08:24 +0100

    Opravdu nový table Loans - pro historii
    fixed #15

---
+++ database/migrations/2021_01_11_190535_create_loans_history.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class LoansHistory extends Migration
+{
+    public function up()
+    {
+        Schema::create('loans_history', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('user');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('item');
+            $table->string('categories');
+            $table->string('name');
+            $table->string('note');
+            $table->string('place');
+            $table->string('inventory_number');
+            $table->string('rent_from');
+            $table->string('rent_to');
+
+
+        });
+    }
+
+
+}
