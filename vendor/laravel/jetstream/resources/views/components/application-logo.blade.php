f38a64f76e405e9ac3f1b7ddc3e0134342dcc4ad
KAČENKA <zabry@gapps.zcu.cz>
2021-01-17 15:07:51 +0100

    LOGIN-css-oprava svg - logo malé

--- vendor/laravel/jetstream/resources/views/components/application-logo.blade.php
+++ vendor/laravel/jetstream/resources/views/components/application-logo.blade.php
@@ -3,7 +3,7 @@
 {{--  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>--}}
 {{--  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>--}}
 {{--</svg>--}}
-<svg class="m-auto " xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="37.45mm" height="11.101mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+<svg class="m-auto" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="37.45mm" height="11.101mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
      viewBox="0 0 3745 1110.1"
      xmlns:xlink="http://www.w3.org/1999/xlink" >
  <defs>
