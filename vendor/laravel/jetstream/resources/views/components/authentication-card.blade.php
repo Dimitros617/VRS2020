344ab48157a7749e16cc072e533e4f5399f4b277
KAČENKA <zabry@gapps.zcu.cz>
2021-01-18 12:46:28 +0100

    login blade css- nenalepený tlačítka, velikostně malé

--- vendor/laravel/jetstream/resources/views/components/authentication-card.blade.php
+++ vendor/laravel/jetstream/resources/views/components/authentication-card.blade.php
@@ -2,7 +2,7 @@
     <div>
     </div>
 
-    <div class="h-75 w-19 mt-6 px-4 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg rounded">
+    <div class="h-36rem w-20rem mt-6 px-4 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg rounded">
         {{ $slot }}
     </div>
 </div>
